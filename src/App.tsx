92dfc11996830158a3bc96d34541ce13bf08b6bf
akio <120174089+akio194@users.noreply.github.com>
2026-05-27 05:17:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { SignInLayout } from "./pages/public/SignInLayout";
 import { SignUpLayout } from "./pages/public/SignUpLayout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signin" element={<SignInLayout />} />
-                <Route path="/signup" element={<SignUpLayout />} />
-                <Route path="*" element={<SignInLayout />} />
-            </Routes>
-        </BrowserRouter>
-    );
+    const path = window.location.pathname;
+
+    return path === "/signup" ? <SignUpLayout /> : <SignInLayout />;
 }
 
 export default App;
